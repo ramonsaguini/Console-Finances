4cc9c89e3cbb7c2b9bc62d5c55633b2fa255820d
Ramon Andrade <124772095+ramonsaguini@users.noreply.github.com>
2023-12-08 09:25:56 +0000

    decreade

--- starter/index.js
+++ starter/index.js
@@ -106,10 +106,13 @@ console.log("Average Change: $" + (sum / finances.length).toFixed(2));
 */
 
 ////////////////! Greatest value
+finances.sort((a, b) => b[1] - a[1]);
 
+console.log("Greatest Increase in Profits/Losses: ", finances[0]);
 
-
-// console.log("Greatest Increase in Profits/Losses: " + );
 
 // console.log("Greateste Decrease in Profits/Losses: " + );
+finances.sort((a, b) => a[1] - b[1]);
+
+console.log("Greatest Decrease in Profits/Losses: ", finances[0]);
 
